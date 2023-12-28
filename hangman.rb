99a63a901b81adc0c269e4d30fdd82a187e57211
Kenzo Aaron Dela Cruz <kenzo.delacruz03@gmail.com>
2023-12-28 23:59:40 +0800

    YAML making file in progress

--- hangman.rb
+++ hangman.rb
@@ -1,17 +1,36 @@
 require 'csv'
+require 'yaml'
+
 
 class Player
-    attr_accessor :name, :tries
+    attr_accessor :name, :tries, :word, :guess
     
 
-    def initialize(name)
+    def initialize(name, word, guess)
         @name = name
         @tries = 5
+        @word = word
+        @guess = guess
     end
 
     def subtract_tries()
         @tries -= 1
         return @tries
+    end
+
+    def to_yaml
+        YAML.dump({
+            :name => @name,
+            :tries => @tries,
+            :word => @word,
+            :guess => @guess,
+        })
+    end
+
+    def self.from_yaml(string)
+        data = YAML.load string
+        p data
+        self.new(data[:name], data[:tries], data[:word], data[:guess])
     end
 
 end
@@ -36,54 +55,103 @@ def get_word(lists)
 
 end
 
-def hangman(word, player)
+def hangman(player)
 
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    print "#{word}\n"
-    word = word.split("")
-    tempWord = word.map {|letter| letter = "_"}
+    print "#{player.word}\n"
+    word = player.word
+    words = word.split("")
 
-    while player.tries > 0 || gameComplete != true
+    gameComplete = false
+
+    while player.tries > 0 && gameComplete == false
 
         available_word = alphabet.join(" ")
 
-        puts available_word
+        puts "\n#{available_word}"
 
-        puts "What letter will you choose?"
+        puts "\nWhat letter will you choose?"
         choice = gets.chomp
         index = 0
 
-        if word.include?(choice) && (alphabet.include?(choice.upcase)) == true
+        if words.include?(choice) && (alphabet.include?(choice.upcase)) == true
             alphabet.delete(choice.upcase)
-            word.each_index do | letter |
-                if word[letter] == choice
-                    tempWord[letter].replace(word[letter])
+            words.each_index do | letter |
+                if words[letter] == choice
+                    player.guess[letter].replace(words[letter])
                 end
             end
         
         elsif (alphabet.include?(choice.upcase)) == false
-            puts "Wrong input. Try again"
+            puts "\nWrong input. Try again"
         else
             alphabet.delete(choice.upcase)
             player.subtract_tries
-            puts "Wrong. You have #{player.tries} left"
+            puts "\nWrong. You have #{player.tries} left"
         end
 
-        print "#{tempWord.join(" ")}\n"
+        print "\n#{player.guess.join(" ")}"
+
+        puts "\nPress 1 to Save the Game and Press any character to continue the game"
+        answer = gets.chomp
+        puts answer
+
+        if answer == '1'
+            player.to_yaml
+            player.tries = 0
+            puts player.tries
+            gameComplete = true
+
+        else
+            next
+
+        end
         
+        if player.tries == 0
+            puts "\nYou Lose!"
+            puts "The word is #{words.join("")}"
+        end
+
+        if player.guess.join("").include?('_') == false
+            gameComplete = true
+            player.tries = 0
+            puts player.tries
+            puts "You Win!"
+        end
+
 
     end
     
 end
 
+
+# Ask for the Player's choice
+
+puts "Press 1 to play a New Game, Press 2 to Load a Saved Game"
+answer = gets.chomp
+
+if answer == '1'
+    continue
+elsif answer == '2'
+    p = Player.from_yaml(p.to_yaml)
+    puts "Name: #{p.name}"
+    puts "Tries: #{p.tries}"
+    puts "Guess: #{p.guess}"
+
+end
+
+
 dictionary = CSV.read('google-10000-english-no-swears.txt')
 
 word = get_word(dictionary)
+words = word.split("")
+tempWord = words.map {|letter| letter = "_"}
 
 puts "What's your name? "
 
+
 name = gets.chomp
 
-player = Player.new(name)
+player = Player.new(name, word, tempWord)
 
-hangman(word, player)
+hangman(player)
